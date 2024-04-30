e8bdec9392397d513b69f3a0a4066b3f640aec3f
Stanislav Vlaev <stanislav.vlaev@gmail.com>
2024-04-30 16:18:33 +0300

    Initial project setup

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Stanislav Vlaev on 30.04.24.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
